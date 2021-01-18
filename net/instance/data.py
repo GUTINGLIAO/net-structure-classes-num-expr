aeff53ea9d99405fd6aa1b274463325ce17fe6ca
GUTINHLIAO <gutingliao@163.com>
2021-01-18 14:47:28 +0800

    增加ImageNet数据集处理

--- net/instance/data.py
+++ net/instance/data.py
@@ -2,9 +2,10 @@ from enum import Enum
 from typing import Any
 from torch.utils.data import DataLoader
 
-from torchvision.datasets import CIFAR10
+from torchvision.datasets import CIFAR10, ImageNet, VisionDataset
 
 from net.instance.common_configuration import transform, DATASET_ROOT
+
 
 class DatasetType(Enum):
     CIFAR10 = 0
@@ -14,7 +15,7 @@ class DatasetType(Enum):
 class DataLoaderFactory:
     """Factory to provide a data loader that has been processed.
 
-    The
+    The main function is to filter classes and pack different datasets.
     """
 
     @classmethod
@@ -25,12 +26,47 @@ class DataLoaderFactory:
             cls._filter_classes(class_num, data_set)
             return DataLoader(data_set, batch_size=4, shuffle=True, num_workers=2)
 
+        if dataset_type == DatasetType.IMAGENET:
+            split: str = 'train' if train else 'val'
+            data_set = ImageNet(root=DATASET_ROOT,
+                                split=split,
+                                transform=transform)
+            cls._filter_classes(class_num, data_set)
+            return DataLoader(data_set, batch_size=4, shuffle=True, num_workers=2)
+
     @classmethod
     def _filter_classes(cls, class_num, data_set):
-        data_new: Any = []
-        targets_new = []
+        imgs = cls._get_imgs(data_set)
+        # In all subclass of VisionDataset, targets have no other names, so there is no need to create a reference
+        imgs_new: Any = []
+        labels_new = []
         for i in range(data_set.__len__()):
             if data_set.targets[i] in tuple(range(class_num)):
-                data_new.append(data_set.data[i])
-                targets_new.append(data_set.targets[i])
-        data_set.data, data_set.targets = data_new, targets_new
+                imgs_new.append(imgs[i])
+                labels_new.append(data_set.targets[i])
+
+        cls._replace(data_set, imgs_new, labels_new)
+
+    @classmethod
+    def _replace(cls, data_set, imgs_new, labels_new):
+        if isinstance(data_set, CIFAR10):
+            data_set.data, data_set.targets = imgs_new, labels_new
+        if isinstance(data_set, ImageNet):
+            data_set.imgs, data_set.targets = imgs_new, labels_new
+
+    @classmethod
+    def _get_imgs(cls, data_set: VisionDataset):
+        """Get different imgs from different dataset.
+
+        This method is aimed to deal with the different expression of imgs(image path and class index)
+        in different subclass of VisionDataset. In CIFAR10, the imgs is called data. In ImageNet, it is
+        called imgs.
+
+        :param data_set: Different dataset.
+        :return: List of (image path, class_index) tuples.
+        """
+        if isinstance(data_set, CIFAR10):
+            return data_set.data
+        if isinstance(data_set, ImageNet):
+            return data_set.imgs
+
