4227cf116c844befe372d7b630ba553cbbc65bf1
GUTINHLIAO <gutingliao@163.com>
2021-01-18 14:46:56 +0800

    移除工厂

--- cli.py
+++ cli.py
@@ -1,32 +1,30 @@
 import argparse
 
 from net.cnn import Cnn
-from net.instance import NetFactory
-from net.type import NetType
+from net import Net
 
 # TODO use docopt
 net_type_dict = {
-    1: NetType.SIMPLE_CNN_CIFAR10_2_CLASSES,
-    2: NetType.SIMPLE_CNN_CIFAR10_3_CLASSES,
-    3: NetType.SIMPLE_CNN_CIFAR10_4_CLASSES,
-    4: NetType.SIMPLE_CNN_CIFAR10_5_CLASSES,
-    5: NetType.SIMPLE_CNN_CIFAR10_10_CLASSES,
+    1: Net.SIMPLE_CNN_CIFAR10_2_CLASSES,
+    2: Net.SIMPLE_CNN_CIFAR10_3_CLASSES,
+    3: Net.SIMPLE_CNN_CIFAR10_4_CLASSES,
+    4: Net.SIMPLE_CNN_CIFAR10_5_CLASSES,
+    5: Net.SIMPLE_CNN_CIFAR10_10_CLASSES,
 }
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--net', required=True, type=int,
-                    help='Appoint the type of net that you want to get. Examples: %s' % str(net_type_dict))
+                    help='Appoint the type of net that you want to get.')
 parser.add_argument('--oper', required=True, type=str,
-                    help='Input train means you want to train the net, test means you want to test the net')
+                    help='Input train means you want to train the net, test means you want to test the net.')
 
 argv = parser.parse_args()
 
-net_type = net_type_dict.get(argv.net)
-
-net:Cnn = NetFactory.build(net_type)
+net: Cnn = net_type_dict.get(argv.net).value
 
 if argv.oper == 'train':
     net.train()
 
 if argv.oper == 'test':
     net.test()
+
