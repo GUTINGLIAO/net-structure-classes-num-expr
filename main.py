87872f9f2b9bee6efb46e963d0ae2d7808452bf4
GUTINHLIAO <gutingliao@163.com>
2021-01-26 12:41:09 +0800

    修改测试

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from net.cnn import Cnn
-from net.instance import simple_cnn_cifar10_instance_2_classes
+from net.instance import resnet_18_cifar10_instance_2_classes
 
 if __name__ == '__main__':
-    net: Cnn = simple_cnn_cifar10_instance_2_classes
-    net.test()
+    net: Cnn = resnet_18_cifar10_instance_2_classes
+    net.train()
