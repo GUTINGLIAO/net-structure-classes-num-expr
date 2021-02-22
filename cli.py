648f1d924ac17f2ac2381a881faaa5ded5023741
GUTINHLIAO <gutingliao@163.com>
2021-02-22 16:52:11 +0800

    增加自定义参数

--- cli.py
+++ cli.py
@@ -2,9 +2,12 @@ import click
 
 from net.cnn import Cnn
 from net.instance import NetType, NetBuilder, instance_dict
+from net.structure.resnet import ResnetType
+from net.structure.efficient_net import EfficientNetType
 from net.instance.data import DatasetType
 
 
+# TODO 增加其余配置，epoch，lr等
 @click.group()
 def cli():
     pass
@@ -12,8 +15,16 @@ def cli():
 
 @click.command()
 def resource():
-    print('net type')
+    print('net')
     for _type in NetType:
+        print('name: %s, index: %d' % (_type.name, _type.value))
+
+    print('resnet_type')
+    for _type in ResnetType:
+        print('name: %s, index: %d' % (_type.name, _type.value))
+
+    print('efficient_net_type')
+    for _type in EfficientNetType:
         print('name: %s, index: %d' % (_type.name, _type.value))
 
     print('operation')
@@ -31,11 +42,14 @@ def resource():
 
 # FIXME 是否有更好的实现
 @click.command()
-@click.option('--net', default=-1, help='Appoint the type of net.')
+@click.option('--net', default=-1, help='Appoint net.')
+@click.option('--type', default=-1, help='Appoint specific net type if neccessary')
 @click.option('--classes_num', help='Appoint the num of classes.')
 @click.option('--dataset', help='Appoint the type of dataset.')
 @click.option('--instance', default=-1, help='Appoint existed instance.')
-def train(net: int, dataset: int, classes_num: int, instance: int):
+@click.option('--lr', default=0.00001, help='Appoint learning rate, default is 0.00001.')
+@click.option('--epoch', default=100, help='Appoint learning epoch, default is 100.')
+def train(net: int, type: int, dataset: int, classes_num: int, instance: int, lr: int):
     if net == -1 and instance == -1:
         raise ValueError('Please create a new instance or appoint a existed instance.')
     if net == -1:
@@ -43,13 +57,38 @@ def train(net: int, dataset: int, classes_num: int, instance: int):
         net_instance.train()
         return
     if net == NetType.SIMPLE_NET.value:
-        NetBuilder.simple_net_instance(classes_num=classes_num, data_set_type=DatasetType.convert(dataset))
+        NetBuilder.simple_net_instance(classes_num=classes_num, data_set_type=DatasetType.convert(dataset),
+                                       learning_rate=lr)
+        return
+
+    if type == -1:
+        raise ValueError('Please appoint a specific net type')
+
+    if net == NetType.RESNET.value:
+        NetBuilder.resnet_instance(classes_num=classes_num, data_set_type=DatasetType.convert(dataset),
+                                   resnet_type=ResnetType.convert(type), learning_rate=lr)
+        return
+
+    if net == NetType.EFFICIENT_NET.value:
+        NetBuilder.efficient_net_instance(classes_num=classes_num, data_set_type=DatasetType.convert(dataset),
+                                          efficient_net_type=EfficientNetType.convert(type), learning_rate=lr)
 
 
+# FIXME 是否有更好的实现
 @click.command()
-@click.option('--model', required=True, type=int, help='Appoint the model that you want to test.')
-def test():
-    pass
+@click.option('--net', default=-1, help='Appoint the type of net.')
+@click.option('--classes_num', help='Appoint the num of classes.')
+@click.option('--dataset', help='Appoint the type of dataset.')
+@click.option('--instance', default=-1, help='Appoint existed instance.')
+def test(net: int, dataset: int, classes_num: int, instance: int):
+    if net == -1 and instance == -1:
+        raise ValueError('Please create a new instance or appoint a existed instance.')
+    if net == -1:
+        net_instance: Cnn = instance_dict[instance][1]
+        net_instance.test()
+        return
+    if net == NetType.SIMPLE_NET.value:
+        NetBuilder.simple_net_instance(classes_num=classes_num, data_set_type=DatasetType.convert(dataset))
 
 
 cli.add_command(resource)
