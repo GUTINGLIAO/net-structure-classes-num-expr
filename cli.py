52a5cc2dcdfea07563c2dad1243f120fbdc4a259
GUTINHLIAO <gutingliao@163.com>
2021-02-23 16:07:57 +0800

    提取方法

--- cli.py
+++ cli.py
@@ -7,7 +7,6 @@ from net.structure.efficient_net import EfficientNetType
 from net.instance.data import DatasetType
 
 
-# TODO 增加其余配置，epoch，lr等
 @click.group()
 def cli():
     pass
@@ -43,36 +42,43 @@ def resource():
 # FIXME 是否有更好的实现
 @click.command()
 @click.option('--net', default=-1, help='Appoint net.')
-@click.option('--type', default=-1, help='Appoint specific net type if neccessary')
-@click.option('--classes_num', help='Appoint the num of classes.')
-@click.option('--dataset', help='Appoint the type of dataset.')
+@click.option('--kind', default=-1, help='Appoint specific net type if neccessary')
+@click.option('--classes_num', default=2, help='Appoint the num of classes.')
+@click.option('--dataset', default=1, help='Appoint the type of dataset.')
 @click.option('--instance', default=-1, help='Appoint existed instance.')
 @click.option('--lr', default=0.00001, help='Appoint learning rate, default is 0.00001.')
 @click.option('--epoch', default=100, help='Appoint learning epoch, default is 100.')
-def train(net: int, type: int, dataset: int, classes_num: int, instance: int, lr: int, epoch: int):
+def train(net: int, kind: int, dataset: int, classes_num: int, instance: int, lr: int, epoch: int):
     if net == -1 and instance == -1:
         raise ValueError('Please create a new instance or appoint a existed instance.')
-    if net == -1:
-        net_instance: Cnn = instance_dict[instance][1]
-        net_instance.train()
-        return
-    if net == NetType.SIMPLE_NET.value:
-        NetBuilder.simple_net_instance(classes_num=classes_num, data_set_type=DatasetType.convert(dataset),
-                                       learning_rate=lr, epoch=epoch)
-        return
 
-    if type == -1:
+    net_instance = _obtain_instance(classes_num, dataset, epoch, instance, kind, lr, net)
+
+    net_instance.train()
+    net_instance.test()
+
+
+def _obtain_instance(classes_num, dataset, epoch, instance, kind, lr, net):
+    if net == -1:
+        return instance_dict[instance][1]
+    if net == NetType.SIMPLE_NET.value:
+        return NetBuilder.simple_net_instance(classes_num=classes_num,
+                                              data_set_type=DatasetType.convert(dataset),
+                                              learning_rate=lr, epoch=epoch)
+    if kind == -1:
         raise ValueError('Please appoint a specific net type')
 
     if net == NetType.RESNET.value:
-        NetBuilder.resnet_instance(classes_num=classes_num, data_set_type=DatasetType.convert(dataset),
-                                   resnet_type=ResnetType.convert(type), learning_rate=lr, epoch=epoch)
-        return
-
-    if net == NetType.EFFICIENT_NET.value:
-        NetBuilder.efficient_net_instance(classes_num=classes_num, data_set_type=DatasetType.convert(dataset),
-                                          efficient_net_type=EfficientNetType.convert(type), learning_rate=lr,
+        return NetBuilder.resnet_instance(classes_num=classes_num,
+                                          data_set_type=DatasetType.convert(dataset),
+                                          resnet_type=ResnetType.convert(kind), learning_rate=lr,
                                           epoch=epoch)
+    if net == NetType.EFFICIENT_NET.value:
+        return NetBuilder.efficient_net_instance(classes_num=classes_num,
+                                                 data_set_type=DatasetType.convert(dataset),
+                                                 efficient_net_type=EfficientNetType.convert(kind),
+                                                 learning_rate=lr,
+                                                 epoch=epoch)
 
 
 # FIXME 是否有更好的实现
