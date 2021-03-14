92018f5d07da62ef8fae8bad176d61d2e72c359f
GUTINHLIAO <gutingliao@163.com>
2021-03-14 22:57:16 +0800

    去掉标准化

--- net/instance/common_configuration.py
+++ net/instance/common_configuration.py
@@ -7,7 +7,28 @@ __all__ = ('DATASET_ROOT', 'transform', 'loss_criterion')
 DATASET_ROOT = Path.joinpath(Path(__file__).parents[2], 'data')
 
 # Normalize images in dataset from [0,1] to [-1,-1].
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+transform = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),  # 先四周填充0，在吧图像随机裁剪成32*32
+    transforms.RandomHorizontalFlip(),  # 图像一半的概率翻转，一半的概率不翻转
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),  # R,G,B每层的归一化用到的均值和方差
+])
+
 
 loss_criterion: Module = CrossEntropyLoss()
+
+
+def transform0():
+    return transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+
+def transform1():
+    return transforms.Compose([
+        transforms.RandomCrop(32, padding=4),  # 先四周填充0，在吧图像随机裁剪成32*32
+        transforms.RandomHorizontalFlip(),  # 图像一半的概率翻转，一半的概率不翻转
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),  # R,G,B每层的归一化用到的均值和方差
+    ])
+
+
 
